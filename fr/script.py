a4de8416dc062e8a33b208fb00622200ff2b121e
FolkloR33 <146537274+FolkloR33@users.noreply.github.com>
2023-11-10 03:36:01 +0100

    import

--- fr/script.py
+++ fr/script.py
@@ -1,45 +1,53 @@
 import os
-from bs4 import BeautifulSoup
 from tqdm import tqdm
-import shutil
 
-def replace_text_in_html(file_path, old_lines, new_lines):
+def replace_text_in_html(file_path, old_text, new_text):
     with open(file_path, 'r', encoding='utf-8') as file:
-        content = file.readlines()
+        content = file.read()
 
-    for i, line in enumerate(content):
-        for old_line, new_line in zip(old_lines, new_lines):
-            if old_line in line:
-                content[i] = line.replace(old_line, new_line)
+    # Remplace l'ancien texte par le nouveau texte dans le contenu
+    modified_content = content.replace(old_text, new_text)
 
+    # Écrit le contenu modifié dans le fichier
     with open(file_path, 'w', encoding='utf-8') as file:
-        file.writelines(content)
+        file.write(modified_content)
 
 def process_html_files(directory, old_file_path, new_file_path):
-    # Copier les fichiers old et new pour éviter les modifications indésirables
-    shutil.copy2(old_file_path, 'old_file_temp.html')
-    shutil.copy2(new_file_path, 'new_file_temp.html')
+    # Lit les anciens et nouveaux textes depuis les fichiers
+    with open(old_file_path, 'r', encoding='utf-8') as old_file:
+        old_text = old_file.read()
 
-    with open('old_file_temp.html', 'r', encoding='utf-8') as old_file:
-        old_lines = old_file.readlines()
+    with open(new_file_path, 'r', encoding='utf-8') as new_file:
+        new_text = new_file.read()
 
-    with open('new_file_temp.html', 'r', encoding='utf-8') as new_file:
-        new_lines = new_file.readlines()
+    # Obtient la liste de tous les fichiers HTML dans le répertoire et ses sous-répertoires
+    html_files = find_html_files(directory)
+    total_files = len(html_files)
 
-    file_list = [file for file in os.listdir(directory) if file.endswith('.html')]
-    
-    for filename in tqdm(file_list, desc='Progress', unit='file'):
-        file_path = os.path.join(directory, filename)
-        replace_text_in_html(file_path, old_lines, new_lines)
+    # Initialise la barre de progression
+    progress_bar = tqdm(total=total_files, desc='Progress', unit='file')
 
-    # Supprimer les fichiers temporaires
-    os.remove('old_file_temp.html')
-    os.remove('new_file_temp.html')
+    # Parcours tous les fichiers HTML
+    for file_path in html_files:
+        replace_text_in_html(file_path, old_text, new_text)
+        # Met à jour la barre de progression
+        progress_bar.update(1)
+
+    # Ferme la barre de progression
+    progress_bar.close()
+
+def find_html_files(directory):
+    html_files = []
+    for foldername, subfolders, filenames in os.walk(directory):
+        for filename in filenames:
+            if filename.endswith('.html'):
+                html_files.append(os.path.join(foldername, filename))
+    return html_files
 
 if __name__ == "__main__":
-    root_directory = "Z:/"
-    old_file_path = "Z:/old_file.html"
-    new_file_path = "Z:/new_file.html"
+    root_directory = "Z:/OnlineDB/fr/"
+    old_file_path = "Z:/OnlineDB/fr/old_file.html"
+    new_file_path = "Z:/OnlineDB/fr/new_file.html"
 
     process_html_files(root_directory, old_file_path, new_file_path)
 
